7b66f5335794c0926cd51d64f54e5ffcb20ee480
Walter Shands <wshands@gmail.com>
2016-11-13 21:20:20 -0800

    Create dockstore_tool_runner.py

---
+++ dockstore_tool_runner.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python 
+from __future__ import print_function, division
+""" 
+    author Walt Shands 
+    jshands@ucsc.com 
+        
+
+
+"""
+import sys, argparse, os
+import time
+import subprocess
+import datetime
+
+from distutils.dir_util import copy_tree
+
+def parse_arguments():
+    """
+    Parse Command Line
+    """
+    parser = argparse.ArgumentParser(description='Script to launch a Dockstore'
+                               ' container from a Dockstore launched container')
+    parser.add_argument('-i', '--docker_image', type=str, required=True,
+                        help='Path to Docker image, e.g. /path/to/image to use to launch container')
+
+    parser.add_argument('-j', '--json_file', type=str, required=True,
+                        help='Path to JSON file, e.g. /path/to/JSON input file for container to launch')
+
+    parser.add_argument('-d', '--tmpdir', type=str, required=True,
+                        help="Path to tmpdir, e.g. /path/to/temporary directory for"
+                        " container to write intermediate files.")
+
+    options = parser.parse_args()
+
+
+    return (options)
+
+def __main__(args):
+    """
+    """
+    start_time = time.time()
+
+    options = parse_arguments()
+
+    date_and_time_info = datetime.datetime.now()
+    date_and_time = date_and_time_info.strftime("%d/%m/%y %H:%M")
+    print("Current date and time:",date_and_time)
+
+    #set the containers TMPDIR env variable to the same value as on the host so
+    #the files written by dockstore in creating this container will be in the
+    #place as those written by the container created by the dockstore command
+    #belowi
+    print("current OS env TMPDIR:", os.environ["TMPDIR"])
+    os.environ["TMPDIR"] = options.tmpdir
+    print("setting TMPDIR to:", os.environ["TMPDIR"])
+    print("new OS env TMPDIR:", os.environ["TMPDIR"])
+
+    #copy the contents of /home/ubuntu into the $HOME directory if different
+    #so all the Dockstore files are available to the current user 
+    #presumably $HOME is the current users HOME directory???
+    #Docstore files were installed in the ubuntu users HOME directory in 
+    #the Dockerfile when the image was built
+    fromDirectory = "/home/ubuntu/Dockstore"
+    toDockstoreDirectory = os.environ["HOME"] + "/Dockstore"
+    if not os.path.isdir(toDockstoreDirectory) and fromDirectory != toDockstoreDirectory:
+        copy_tree(fromDirectory, toDockstoreDirectory)
+    fromDirectory = "/home/ubuntu/.dockstore"
+    toDirectory = os.environ["HOME"] + "/.dockstore"
+    if not os.path.isdir(toDirectory) and fromDirectory != toDirectory:
+        copy_tree(fromDirectory, toDirectory)
+
+
+# Put the Docstore client on the path
+#export PATH=/home/ubuntu/Dockstore/:$PATH
+#export PATH=$HOME/bin:$PATH
+    os.environ["PATH"] = toDockstoreDirectory + ":" + os.environ["PATH"]
+
+    #example of command that will be launched:
+    #dockstore tool launch --entry quay.io/wshands/fastqc --json /home/ubuntu/fastqcwork/fastqc/fastqc.json
+#    cmd = ["dockstore", "tool", "launch", "--entry", options.docker_image, "--json", options.json_file]
+    cmd = ["dockstore", "tool", "launch", "--debug", "--entry", options.docker_image, "--json", options.json_file]
+
+
+    my_env = os.environ.copy()
+    my_env["TMPDIR"] = options.tmpdir
+
+    print("command to run:\n",cmd)
+    output = subprocess.call(cmd, env=my_env)
+    print("dockstore command call output is:\n", output)
+
+                        
+
+    
+                        
+
+    
+    print("----- %s seconds -----" % (time.time() - start_time), file=sys.stderr)
+                        
+if __name__=="__main__":
+     sys.exit(__main__(sys.argv))
+
